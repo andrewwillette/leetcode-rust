0cb89cd018bac49f2c18c66cc5fa913df0b7a505
andrewwillette <willette.andrew1846@gmail.com>
2023-03-26 22:19:20 -0500

    master: build array from perm

---
+++ src/build_array_from_perm.rs
@@ -0,0 +1,23 @@
+impl Solution {
+    /*
+    # Leetcode problem # 1920
+    https://leetcode.com/problems/build-array-from-permutation/
+    */
+    pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
+        (0..nums.len()).map(|i| nums[nums[i] as usize]).collect()
+    }
+}
+
+pub struct Solution;
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_left_rigth_difference() {
+        assert_eq!(
+            vec![0, 1, 2, 4, 5, 3],
+            super::Solution::build_array(vec![0, 2, 1, 5, 3, 4])
+        );
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 // #![feature(test)]
 // #![feature(proc_macro_hygiene)]
+pub mod build_array_from_perm;
 pub mod concat_array;
 pub mod left_right_difference;
