5c5c471b778ccb047bad252291f96dea9b0c5ac8
andrewwillette <willette.andrew1846@gmail.com>
2023-04-19 17:58:13 -0500

    master: working on longest_zig_zag

--- src/longest_zig_zag.rs
+++ src/longest_zig_zag.rs
@@ -2,13 +2,33 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 /// https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/description/
+/// Not solved yet
 impl Solution {
     pub fn longest_zig_zag(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        return 0;
+        if let Some(root) = root {
+            return std::cmp::max(
+                Self::dfs(root.borrow().left, true, 0),
+                Self::dfs(root.borrow().right, false, 0),
+            );
+        } else {
+            return 0;
+        }
     }
 
-    pub fn dfs(root: Option<Rc<RefCell<TreeNode>>>, isLeft: bool, count: i32) -> i32 {
-        return 0;
+    pub fn dfs(root: Option<Rc<RefCell<TreeNode>>>, is_left: bool, count: i32) -> i32 {
+        if let Some(root) = root {
+            if is_left {
+                return std::cmp::max(
+                    Self::dfs(root.borrow().left, true, 0),
+                    Self::dfs(root.borrow().right, false, count + 1),
+                );
+            } else {
+                return std::cmp::max(
+                    Self::dfs(root.borrow().left, true, count + 1),
+                    Self::dfs(root.borrow().right, false, 0),
+                );
+            }
+        }
     }
 }
 
