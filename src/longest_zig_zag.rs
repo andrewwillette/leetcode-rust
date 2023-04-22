1ef1c48c1190d50b575d4c97ea9c964c604ef105
andrewwillette <willette.andrew1846@gmail.com>
2023-04-22 09:15:49 -0500

    master: longest_zig_zag solved

--- src/longest_zig_zag.rs
+++ src/longest_zig_zag.rs
@@ -1,34 +1,20 @@
 use std::cell::RefCell;
 use std::rc::Rc;
+type OptNode = Option<Rc<RefCell<TreeNode>>>;
 
 /// https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/description/
-/// Not solved yet
 impl Solution {
-    pub fn longest_zig_zag(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        if let Some(root) = root {
-            return std::cmp::max(
-                Self::dfs(root.borrow().left, true, 0),
-                Self::dfs(root.borrow().right, false, 0),
-            );
-        } else {
-            return 0;
-        }
-    }
-
-    pub fn dfs(root: Option<Rc<RefCell<TreeNode>>>, is_left: bool, count: i32) -> i32 {
-        if let Some(root) = root {
-            if is_left {
-                return std::cmp::max(
-                    Self::dfs(root.borrow().left, true, 0),
-                    Self::dfs(root.borrow().right, false, count + 1),
-                );
-            } else {
-                return std::cmp::max(
-                    Self::dfs(root.borrow().left, true, count + 1),
-                    Self::dfs(root.borrow().right, false, 0),
-                );
+    pub fn longest_zig_zag(root: OptNode) -> i32 {
+        fn dfs(node: &OptNode, l_count: i32, r_count: i32) -> i32 {
+            match node.as_ref() {
+                Some(n) => {
+                    let b = n.borrow();
+                    dfs(&b.left, r_count + 1, 0).max(dfs(&b.right, 0, l_count + 1))
+                }
+                None => l_count.max(r_count),
             }
         }
+        dfs(&root, 0, 0) - 1
     }
 }
 
@@ -47,6 +33,18 @@ impl TreeNode {
             left: None,
             right: None,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_longest_zig_zag() {
+        let mut tree = TreeNode::new(1);
+        tree.left = Some(Rc::new(RefCell::new(TreeNode::new(1))));
+        let longest_zig_zag = Solution::longest_zig_zag(Some(Rc::new(RefCell::new(tree))));
     }
 }
 
